39a86bf11b4b32eb6816f2e7ee48c007e8b30698
aleewains <aleewains@gmail.com>
2026-01-24 16:01:25 +0500

    add todo button fixed

--- src/contextApi-localStorage/components/TodoForm.tsx
+++ src/contextApi-localStorage/components/TodoForm.tsx
@@ -29,7 +29,7 @@ function TodoForm() {
       />
       <button
         type="submit"
-        className="bg-green-600 hover:bg-green-700 cursor-pointer active:scale-95 text-white font-semibold px-6 py-2.5 transition-all duration-200 flex items-center justify-center gap-2 shrink-0"
+        className="bg-green-600 hover:bg-green-700 cursor-pointer active:scale-95 text-white font-semibold px-6 py-2.5 transition-all duration-200 flex items-center justify-center gap-2 shrink-0 border-r-0 rounded-r-lg"
       >
         <span>Add</span>
         <svg
