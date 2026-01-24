e9e8b5f676f51b3277c498c8c4c20ee7d6f52a4e
aleewains <aleewains@gmail.com>
2026-01-24 16:54:26 +0500

    todo ui fixed and enhanced

--- src/contextApi-localStorage/components/TodoItem.tsx
+++ src/contextApi-localStorage/components/TodoItem.tsx
@@ -80,7 +80,7 @@ function TodoItem({ todo }: TodoItemProps) {
       />
 
       {/* Action Buttons */}
-      <div className="flex gap-2 shrink-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+      <div className="flex gap-2 shrink-0 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-200">
         <button
           className={`w-9 h-9 cursor-pointer rounded-lg flex items-center justify-center transition-all active:scale-90 disabled:opacity-20 ${
             isTodoEditable
